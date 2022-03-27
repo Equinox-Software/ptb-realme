e429a504fb6c9632251658fff0a51ef25081dd41
nyx <loool1912@web.de>
2022-03-27 19:59:23 +0200

    fix

--- utils.py
+++ utils.py
@@ -143,6 +143,8 @@ def check_quote(update: Update) -> bool:
 def get_user_info(update: Update, context: CallbackContext, key: str = None) -> any:
     info = context.bot_data.get(str(update.message.reply_to_message.from_user.id), {WARNINGS: 0, DEVICES: []})
 
+    print(info, key)
+
     if key is None:
         return info
 
